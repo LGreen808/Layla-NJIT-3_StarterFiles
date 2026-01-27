38b6a644f3d8450cdb34c9a481db2e9c4465c024
LGreen808 <526LGreen@frhsd.com>
2026-01-27 00:18:13 +0000

    Still trying to fix a problem

--- site.js
+++ site.js
@@ -25,9 +25,17 @@ const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
       created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
+            fetch('movies.json')
+                  .then(response => {
+                        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+                        return response.json();
+                  })
+                  .then(json => {
+                        this.movies = json;
+                  })
+                  .catch(error => {
+                        console.error('Error loading movies:', error);
+                  });
       },
       data() {
         return {
