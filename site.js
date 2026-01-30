89325298bf15a5fee761d957f50784b99e9419cc
LGreen808 <526LGreen@frhsd.com>
2026-01-30 01:12:05 +0000

    Working pt 6

--- site.js
+++ site.js
@@ -44,7 +44,143 @@ const vue_app = Vue.createApp({
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
             title: "Layla's Top Movies",
             owner: "Layla Green",
-            github: "http://www.github.com/mjslee/is219lee-p3"
+            github: "http://www.github.com/mjslee/is219lee-p3",
+            movies: [
+                {
+    "title": "The Pirate",
+    "iscore": 6.8,
+    "rating": "Not Rated",
+    "runtime": "1 Hour 42 Min",
+    "released": [1948, 5, 20],
+    "video": { "url": "https://www.youtube.com/watch?v=c18WhJb80h4" },
+    "posters": [
+      "img/Poster 1 for the Pirate.jpg",
+      "img/Poster 2 for the Pirate.jpg",
+      "img/Poster 3 for the Pirate.jpg"
+    ],
+    "imdb": "https://www.imdb.com/title/tt0040694/",
+    "likes": 1021,
+    "dislikes": 122,
+    "posterindex": 0
+  },
+  {
+    "title": "Walt Before Mickey",
+    "iscore": 5.9,
+    "rating": "PG",
+    "runtime": "2 Hours 0 Min",
+    "released": [2015, 8, 14],
+    "video": { "url": "https://www.youtube.com/watch?v=MaOZiu-hnTM" },
+    "posters": ["img/Mickey1.jpg", "img/Mickey2.jpg", "img/Mickey3.jpg"],
+    "imdb": "https://www.imdb.com/title/tt3154310/",
+    "likes": 928,
+    "dislikes": 109,
+    "posterindex": 0
+  },
+  {
+    "title": "Jersey Boys",
+    "iscore": 6.8,
+    "rating": "R",
+    "runtime": "2 Hour 14 Min",
+    "released": [2014, 6, 20],
+    "video": { "url": "https://www.youtube.com/watch?v=drpTj5jJXYk&t=6s" },
+    "posters": ["img/Jersey1.jpg", "img/Jersey2.jpg", "img/Jersey3.jpg"],
+    "imdb": "https://www.imdb.com/title/tt1742044/",
+    "likes": 855,
+    "dislikes": 99,
+    "posterindex": 0
+  },
+  {
+    "title": "Saturday Night",
+    "iscore": 6.9,
+    "rating": "R",
+    "runtime": "1 Hour 49 Min",
+    "released": [2024, 8, 31],
+    "video": { "url": "https://www.youtube.com/watch?v=iZ9O_tl5Npk" },
+    "posters": ["img/Saturday1.jpg", "img/Saturday2.jpg", "img/Saturday3.webp"],
+    "imdb": "https://www.imdb.com/title/tt27657135/",
+    "likes": 812,
+    "dislikes": 101,
+    "posterindex": 0
+  },
+  {
+    "title": "Some Like It Hot",
+    "iscore": 8.9,
+    "rating": "PG-13",
+    "runtime": "2 Hour 0 Min",
+    "released": [1959, 3, 19],
+    "video": { "url": "https://www.youtube.com/watch?v=97TYs2YXbJw" },
+    "posters": ["img/Hot1.png", "img/Hot2.jpg", "img/Hot3.jpg"],
+    "imdb": "https://www.imdb.com/title/tt0053291/",
+    "likes": 812,
+    "dislikes": 101,
+    "posterindex": 0
+  },
+  {
+    "title": "Wayne's World",
+    "iscore": 7.0,
+    "rating": "PG-13",
+    "runtime": "1 Hour 34 Min",
+    "released": [1992, 2, 14],
+    "video": { "url": "https://www.youtube.com/watch?v=thyJOnasHVE&list=RDthyJOnasHVE&start_radio=1" },
+    "posters": ["img/Wayne1.jpg", "img/Wayne2.jpg", "img/Wayne3.jpg"],
+    "imdb": "https://www.imdb.com/title/tt0105793/",
+    "likes": 812,
+    "dislikes": 101,
+    "posterindex": 0
+  },
+  {
+    "title": "Rocketman",
+    "iscore": 7.3,
+    "rating": "R",
+    "runtime": "2 Hour 1 Min",
+    "released": [2019, 5, 31],
+    "video": { "url": "https://www.youtube.com/watch?v=oUNeIgFVd5g&list=RDoUNeIgFVd5g&start_radio=1" },
+    "posters": ["img/Rocket1.jpg", "img/Rocket2.jpg", "img/Rocket3.jpg"],
+    "imdb": "https://www.imdb.com/title/tt2066051/",
+    "likes": 812,
+    "dislikes": 101,
+    "posterindex": 0
+  },
+  {
+    "title": "The Man Who Shot Liberty Valance",
+    "iscore": 8.1,
+    "rating": "PG-13",
+    "runtime": "2 Hour 3 Min",
+    "released": [1962, 4, 22],
+    "video": { "url": "https://www.youtube.com/watch?v=hP77V2X1Biw" },
+    "posters": ["img/Ford1.jpg", "img/Ford3.jpg"],
+    "imdb": "https://www.imdb.com/title/tt0056217/",
+    "likes": 812,
+    "dislikes": 101,
+    "posterindex": 0
+  },
+  {
+    "title": "Dr. Dolittle",
+    "iscore": 5.4,
+    "rating": "PG-13",
+    "runtime": "1 Hour 30 Min",
+    "released": [1998, 6, 26],
+    "video": { "url": "https://www.youtube.com/watch?v=LWbtxG-jXMY" },
+    "posters": ["img/Doolittle1.jpg", "img/Doolittle2.jpg", "img/Doolittle3.jpg"],
+    "imdb": "https://www.imdb.com/title/tt0118998/",
+    "likes": 812,
+    "dislikes": 101,
+    "posterindex": 0
+  },
+  {
+    "title": "Pitch Perfect",
+    "iscore": 7.1,
+    "rating": "PG-13",
+    "runtime": "1 Hour 52 Min",
+    "released": [2012, 9, 28],
+    "video": { "url": "https://www.youtube.com/watch?v=8dItOM6eYXY" },
+    "posters": ["img/Pitch1.jpg", "img/Pitch2.avif", "img/Pitch3.jpg"],
+    "imdb": "https://www.imdb.com/title/tt1981677/",
+    "likes": 812,
+    "dislikes": 101,
+    "posterindex": 0
+  }   
+            ]
         }
       },
       methods: {
